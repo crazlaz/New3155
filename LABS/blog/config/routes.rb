4849900d22af05311115f249767eedd9c5d2d6e6
Carlos Uribe <curibe2@uncc.edu>
2019-12-11 15:05:24 +0000

    Add cinnebts routes

--- LABS/blog/config/routes.rb
+++ LABS/blog/config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   get 'welcome/index'
   #For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :articles do 
+    resources :comments
+  end 
 
-  resources :articles 
   root 'welcome#index'
   # for details on the DSL available 
 
