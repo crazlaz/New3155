867c5f9536189727f9b90e66e3e3b82012ef5797
Carlos Uribe <curibe2@uncc.edu>
2019-12-11 12:59:22 +0000

    Complete Create and Show.

--- LABS/blog/app/controllers/articles_controller.rb
+++ LABS/blog/app/controllers/articles_controller.rb
@@ -1,9 +1,20 @@
 class ArticlesController < ApplicationController
     def new
     end
+    
+    def show
+        @article = Article.find(params[:id])
+    end 
 
     def create 
+        @article = Article.new(params[:article_params])
         
-        render plain: params[:article].inspect 
+        @article.save
+        redirect_to @article
     end 
 end 
+
+private 
+    def article_params
+        params.require(:article).permit(:title, :text)
+    end 
