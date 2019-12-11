6ad0e2155be9bf77b55f7f2172d3e45db9d34fbe
Carlos Uribe <curibe2@uncc.edu>
2019-12-11 22:17:57 +0000

    Add destroy functionality for comments

--- LABS/blog/app/controllers/comments_controller.rb
+++ LABS/blog/app/controllers/comments_controller.rb
@@ -5,8 +5,14 @@ class CommentsController < ApplicationController
         redirect_to article_path(@article)
     end
     
+    def destroy
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.find(comment_params)
+        @comment.destory
+        redirect_to article_path(@article)
+    end 
     private 
     def comment_params
         params.require(:comment).permit(:commenter, :body)
     end 
-end
+end 
